fd5635cb7610149fbe24324eb1c6464c3da5ab16
shadowless16 <akdavid4real@gmail.com>
2024-11-12 19:22:52 +0100

    blackjack

--- Capstone_Project_BlackJack_Game/Blackjack_Game.py
+++ Capstone_Project_BlackJack_Game/Blackjack_Game.py
@@ -4,11 +4,9 @@ import random
 deck = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] * 4
 random.shuffle(deck)
 
-# Deal a card from the deck
 def deal_card():
     return deck.pop()
 
-# Calculate the hand value
 def calculate_hand(hand):
     value = 0
     ace_count = 0
@@ -21,13 +19,11 @@ def calculate_hand(hand):
         else:
             value += int(card)
     
-    # if hand is over 21
     while value > 21 and ace_count:
         value -= 10
         ace_count -= 1
     return value
 
-# Initial hands
 player_hand = [deal_card(), deal_card()]
 dealer_hand = [deal_card(), deal_card()]
 
